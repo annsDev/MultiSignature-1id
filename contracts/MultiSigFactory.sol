6f5bc5257a71900e9e695f76c6d3b6ca4c564021
annsDev <annskhalid13@gmail.com>
2023-10-19 14:11:36 +0500

    Major Fixes & Validations Added

--- contracts/MultiSigFactory.sol
+++ contracts/MultiSigFactory.sol
@@ -2,58 +2,55 @@
 pragma solidity ^0.8.10;
 
 /// @title MultiSignature Factory Contract
-/// @author 1id
+/// @author 1id-Factory
 /// @notice Implements the logic for MultiSignature Factory Contract
 /// @dev Allows users to deploy new multisig contract
 
 import "./MultiSigWallet.sol";
 
 contract MultiSigFactory {
-
-
-    /* ========== EVENTS ========== */
-
     event ContractCreated(
         address contractAddress,
-        address[] owners,
+        uint128 maxOwnersLimit,
         uint256 threshold,
-        uint256 dailyLimit
+        uint256 dailyLimit,
+        address[] owners
     );
+
     event Deployer(address deployer);
 
     /// @notice Returns the array of deployed multisig contracts addresses
     MultiSigWallet[] public MultisigArray;
 
-    
-    /// @notice Create a new multisig contract
-    /// @param _deployer The address of the deployer for the new multisig contract
-    /// @param _maxOwners The maximum number of owners that can be added to the multisig
-    /// @param _required The number of required owner confirmations for actions in the multisig
-    /// @param _limit The daily spending limit for the multisig
+
+    /// @notice Creates a new multisig contract
+    /// @param _maxOwnersLimit The maximum number of owners that can be added to the multisig
+    /// @param _threshold The number of required owner confirmations for actions in the multisig
+    /// @param _dailyLimit The daily spending limit for the multisig
     /// @param _owners An array of addresses representing the owners of the multisig
     function CreateNewMultiSig(
-        address _deployer,
-        uint128 _maxOwners,
-        uint256 _required,
-        uint256 _limit,
+        uint128 _maxOwnersLimit,
+        uint256 _threshold,
+        uint256 _dailyLimit,
         address[] memory _owners
     ) external {
         MultiSigWallet multisigContract = new MultiSigWallet(
-            _deployer,
-            _maxOwners,
-            _required,
-            _limit,
+            msg.sender,
+            _maxOwnersLimit,
+            _threshold,
+            _dailyLimit,
             _owners
         );
         MultisigArray.push(multisigContract);
 
         emit ContractCreated({
             contractAddress: address(multisigContract),
-            owners: _owners,
-            threshold: _required,
-            dailyLimit: _limit
+            maxOwnersLimit: _maxOwnersLimit,
+            threshold: _threshold,
+            dailyLimit: _dailyLimit,
+            owners: _owners
         });
 
-        emit Deployer({deployer: _deployer});
+        emit Deployer({deployer: msg.sender});
     }
 }
